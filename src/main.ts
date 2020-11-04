918828928508e6bbcd75118c680672404ba7507c
Marco Santarcangelo <marcosantar93@gmail.com>
2020-11-04 10:53:59 -0300

    pure microservice

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { NATSConfigService } from './config/NATSConfigService';
 import { WinstonModule } from 'nest-winston';
 import { LoggerConfig } from './factory/winstonConfig';
+import { InjectMetadataInterceptor } from './interceptors/InjectMetadataInterceptor';
 
 async function bootstrap() {
   const logger: LoggerConfig = LoggerConfig.getInstance();
   const winstonLogger = WinstonModule.createLogger(logger.console());
 
-  const app = await NestFactory.create(
-    AppModule.register(),
-    {
-      cors: true,
-      logger: winstonLogger
-    }
-  );
-
-  const natsConfigService : NATSConfigService = app.get(NATSConfigService);
-  const configService : ConfigService = app.get<ConfigService>(ConfigService);
-
-  app.connectMicroservice({
-    ...natsConfigService.getNATSConfig
+  const context = await NestFactory.createApplicationContext(AppModule.register(), {
+    logger: winstonLogger
   });
 
-  const port = configService.get<number>('PORT') || 3000;
-  app.startAllMicroservicesAsync();
+  const natsConfigService : NATSConfigService = context.get(NATSConfigService);
 
-  await app.listen(port, () => winstonLogger.log(`Hybrid ms-test test running on port ${port}`));
+  context.close();
+
+  const app = await NestFactory.createMicroservice(AppModule.register(), {
+    ...natsConfigService.getNATSConfig,
+    logger: winstonLogger
+  });
+
+  const globalInterceptors = [
+    new InjectMetadataInterceptor()
+  ];
+
+  app.useGlobalInterceptors(...globalInterceptors);
+
+  app.listen(() => winstonLogger.log('Microservice ms-iban running'));
 
 }
 bootstrap();
